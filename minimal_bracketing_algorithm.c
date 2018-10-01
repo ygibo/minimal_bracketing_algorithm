9bae9f183fa93493596380dafa2a1d1d929fb54b
Yoshiya Gibo <logic.and.puzzle.eq.proof@gmail.com>
2018-10-01 18:13:59 +0900

    changed comment in algorithm file

--- minimal_bracketing_algorithm.c
+++ minimal_bracketing_algorithm.c
@@ -1,18 +1,22 @@
 /* アルゴリズムの概要
+ * 入力　(1*2)+3<<2+(4&6) のような算術子とビット演算子をもつ式
+ * 出力  1*2+3<<2+4&6 のような無駄な括弧を省いた式
+ *
  * 例えば、1+2,3+4 を掛けて (1+2)*(3+4) にする場合、1+2,3+4 をそのまま'*'で結合した場合、
- * 1+2*3+4 となり問題がある。これは、'*'の両隣にある'+'が'*'より優先度が低いためである。
+ * 1+2*3+4 となり意味が変わってしまう。これは、'*'の両隣にある'+'が'*'より優先度が低いためである。
  * そのため、1+2 の '+' に着目したとき、'+'の優先度が'*'より低い場合に括弧が必要になるとわかる。
  * 以上のように、隣同士にある演算子の優先度を比較することで括弧が必要かが分かる。
  * 
  * そこで隣同士にある演算子をより簡単に比較するため、与えられた式を構文解析し、解析木上で
- * 演算子を比較しながら、括弧の無駄がない式を出力する。
+ * 演算子を比較することで括弧が必要かを判断し、無駄な括弧がない式を出力する。
  * 構文解析木上では、各ノードの持つ演算子と親ノードが持つ演算子の優先度を
  * 比較することで、元の式上で隣り合った演算子の優先度を比較することが簡単にできる。
  *
  *
  * コードの概要
- * show_waste_free_expression(Operand prev_op, Node* node)関数が無駄な括弧の無い式を出力する
- * 関数である。この関数では、第一引数に現在のノードの親ノードの演算子、第二引数に
+ * show_waste_free_expression(Operand prev_op, Node* node)関数が構文解析木上の演算子を
+ * 再帰的に比較し、無駄な括弧の無い式を出力する関数である。
+ * この関数では、第一引数に現在のノードの親ノードの演算子、第二引数に
  * 解析木を渡す。そのため、コードの初めのほうのparse_が頭につく関数により、構文解析を行い
  * 解析木を作成する。
  * また、入力としては、一つの式が一行で与えられているものとする。そうでない場合は、
@@ -20,7 +24,8 @@
  * 
  * プログラムへの入力と出力
  * プログラムへの入力は、標準入力から行う。標準入力からの１行分が一つの式として入力される。
- * 出力も標準出力に出力される。
+ * 出力も標準出力に出力される。字句解析を導入することで複数行からなる式も解析できるよう
+ * プログラムを変更できる。
  */
 #define MAX_STRING_SIZE 1024
 #include<stdio.h>
@@ -93,6 +98,7 @@ char get_next(char* parse_obj){
 }
 
 //パーサー関数
+// '|'であらわされる or 構文を解析
 Node* parse_expression(char* parse_obj){
   Node* node = parse_ex_term(parse_obj);
   while( get_next(parse_obj) == '|' ){
@@ -102,6 +108,7 @@ Node* parse_expression(char* parse_obj){
   return node;
 }
 
+// xor 解析
 Node* parse_ex_term(char* parse_obj){
   Node* node = parse_and_term(parse_obj);
   while( get_next(parse_obj) == '^' ){
@@ -111,6 +118,7 @@ Node* parse_ex_term(char* parse_obj){
   return node;
 }
 
+// and を解析
 Node* parse_and_term(char* parse_obj){
   Node* node = parse_shift_term(parse_obj);
   while( get_next(parse_obj) == '&' ){
@@ -120,6 +128,7 @@ Node* parse_and_term(char* parse_obj){
   return node;
 }
 
+// ビットシフトを解析
 Node* parse_shift_term(char* parse_obj){
   Node* node = parse_plus_minus_term(parse_obj);
   while( (get_next(parse_obj) == '<' & parse_obj[exp_index+1] == '<') ||
@@ -137,7 +146,7 @@ Node* parse_shift_term(char* parse_obj){
   return node;
 }
 
-
+// +,- を解析
 Node* parse_plus_minus_term(char* parse_obj){
   Node* node = parse_div_mul_mod_term(parse_obj);
   while( get_next(parse_obj) == '+' || get_next(parse_obj) == '-' ){
@@ -152,6 +161,7 @@ Node* parse_plus_minus_term(char* parse_obj){
   return node;
 }
 
+// 割り算、掛け算、モジュロ演算を解析
 Node* parse_div_mul_mod_term(char* parse_obj){
   Node* node = factor(parse_obj);
   while( get_next(parse_obj) == '*' || get_next(parse_obj) == '/' ||
@@ -170,7 +180,7 @@ Node* parse_div_mul_mod_term(char* parse_obj){
   return node;
 }
 
-
+// 数値を解析
 Node* factor(char* parse_obj){
   Node* node = NULL;
   if( get_next(parse_obj) == '(' ){
